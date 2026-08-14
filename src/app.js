2ccf809e05af135a360c8d6fad015892cc73daf2
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-14 23:04:31 +0500

    Added Loggers & MiddleWare

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import express from 'express';
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
+app.use(cookieParser());
 app.get('/', (req, res) => {
+  logger.info('Received from Acquisitions API');
   res.status(200).send('Hello from Acquisitions!');
 });
 
