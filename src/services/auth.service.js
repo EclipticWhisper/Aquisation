197bd8ac70971ffd0f480ae203f0b17b99fea7bc
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-17 23:32:46 +0500

    Implemented Authentication/Password Hashing

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -8,8 +8,8 @@ export const hashPassword = async (password) => {
 
         return await bcrypt.hash(password, 10);
     } catch (e) {
-        logger.error('Error hashing password:', e);
-        throw new Error('Error hashing password');
+        logger.error(e)(`Error hashing password: ${e}`);
+        throw new Error("Error hashing password");
     }
 }
 
