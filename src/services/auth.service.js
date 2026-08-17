447ae40e0d423a42acf21f177a739e31db0d1f75
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-17 23:27:38 +0500

    Implemented Password Hashing

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,8 @@
 import logger from '#config/logger.js';
 import bcrypt from 'bcrypt';
+import { db } from "#config/database.js";
+import { eq } from 'drizzle-orm';
+import { users } from '#models/user.mode.js';
 export const hashPassword = async (password) => {
     try {
 
@@ -10,4 +13,14 @@ export const hashPassword = async (password) => {
     }
 }
 
-export const createUser = async ({name,email,password,role}) => {
+export const createUser = async ({ name, email, password, role = 'user' }) => {
+    try {
+        const existingUser = db.select().from(users).where(eq(users.email, email)).limit(1);
+        if (existingUser.length > 0) throw new Error("User already exists");
+        const password_hash = await hashPassword(password);
+        const [newUser] = await db.insert(users).values({ name, email, password: password_hash, role }).returning({ id: users.id, name: users.name, email: users.email, role: users.role, created_at: users.created_at });
+        logger.info(`User ${newUser.email} created successfully.`);
+    } catch (e) {
+        logger.error(`Error Creating the user: ${e}`);
+    }
+}
